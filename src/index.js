0e97f09dc5845195c358a823c188de95ba88b8c7
imdhruvkumar <dk0495682@gmail.com>
2026-04-14 13:16:34 +0530

    add controllers(register)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import express from "express";
 dotenv.config({
     path:'./env'
 })
+const app =express()
 
 
 
  
 connectDB()
 .then(()=>{
-    app.listen(procces.env.PORT || 8000,()=>{
+    app.listen(process.env.PORT || 8000,()=>{
         console.log(`server is running on port : ${process.env.PORT}`);
     })
 })
